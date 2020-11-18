acc2ea50a824e7ab1b1fe17d043f380f2345d929
PLHB1712289 <57471680+PLHB1712289@users.noreply.github.com>
2020-11-18 09:26:29 +0700

    try fix deploy to heroku

--- app.js
+++ app.js
@@ -48,9 +48,9 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 
 app.use("/", indexRouter);
-app.use("/board", boardRouter);
-app.use("/auth", authRouter);
-app.use("/user", userRouter);
+// app.use("/board", boardRouter);
+// app.use("/auth", authRouter);
+// app.use("/user", userRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
